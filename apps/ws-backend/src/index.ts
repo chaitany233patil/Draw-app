a2e9f87f235ebca94f1f72bbf7956de7a969a40a
Chaitany233patil <patilchaitany529@gmail.com>
2025-05-04 14:16:43 +0530

    completed basic room logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,16 @@
-import { WebSocketServer } from "ws";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { WebSocketServer, WebSocket } from "ws";
+import jwt, { decode, JwtPayload } from "jsonwebtoken";
 import { JWT_SECRETE } from "../node_modules/@repo/common-backend/src/config";
 
 const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+  userId: String;
+  roomId: String[];
+  ws: WebSocket;
+}
+
+const users: User[] = [];
 
 wss.on("connection", (socket, req) => {
   console.log("Client Connected!");
@@ -12,18 +20,42 @@ wss.on("connection", (socket, req) => {
   try {
     const decoded = jwt.verify(token as string, JWT_SECRETE);
     if (decoded && (decoded as JwtPayload).userid) {
+      users.push({
+        userId: (decoded as JwtPayload).userid,
+        roomId: [],
+        ws: socket,
+      });
+
       socket.on("message", (data) => {
-        const pasrseData = JSON.parse(data.toString());
-        if (pasrseData.type == "join_room") {
+        const parseData = JSON.parse(data.toString());
+        if (parseData.type == "join_room") {
+          const user = users.find(
+            (x) => x.userId == (decoded as JwtPayload).userid
+          );
+          user?.roomId.push(parseData.roomId);
           socket.send("room Join");
         }
 
-        if (pasrseData.type == "chat") {
-          socket.send("chating room");
+        if (parseData.type == "chat") {
+          users.forEach((x) => {
+            if (x.roomId.includes(parseData.roomId)) {
+              x.ws.send(
+                JSON.stringify({
+                  message: parseData.message,
+                })
+              );
+            }
+          });
         }
 
-        if (pasrseData.type == "leave_room") {
-          socket.send("Leave room");
+        if (parseData.type == "leave_room") {
+          const user = users.find(
+            (x) => x.userId == (decoded as JwtPayload).userid
+          );
+          if (user) {
+            user.roomId = user.roomId.filter((x) => x != parseData.roomId);
+            socket.send("room Leave");
+          }
         }
       });
     }
