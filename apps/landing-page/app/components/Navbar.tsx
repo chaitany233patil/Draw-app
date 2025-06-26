4e7159e7857eef27195e8e2549c08464a509d9e9
Chaitany233patil <patilchaitany529@gmail.com>
2025-06-26 18:57:36 +0530

    responsive ui trying

--- apps/landing-page/app/components/Navbar.tsx
+++ apps/landing-page/app/components/Navbar.tsx
@@ -1,17 +1,15 @@
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="min-w-[1300px] flex justify-between p-8">
-        <div className="text-white text-xl border-l-5 pl-3 border-blue-600">
-          DrawSync
+    <div className="flex items-center justify-between sm:py-6 sm:px-12 p-4">
+      <div className="text-white text-xl border-l-5 pl-3 border-blue-600">
+        DrawSync
+      </div>
+      <div className="text-white flex gap-10">
+        <div className="cursor-pointer underline underline-offset-5 decoration-blue-600">
+          Home
         </div>
-        <div className="text-white flex gap-10">
-          <div className="cursor-pointer underline underline-offset-5 decoration-blue-600">
-            Home
-          </div>
-          <div className="cursor-pointer">About</div>
-          <div className="cursor-pointer">Features</div>
-        </div>
+        <div className="cursor-pointer">About</div>
+        <div className="cursor-pointer">Features</div>
       </div>
     </div>
   );
