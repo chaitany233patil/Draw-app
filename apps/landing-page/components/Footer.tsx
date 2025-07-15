ce36ae1c2c4e264b0726cbe2b30f01150457f011
Chaitany233patil <patilchaitany529@gmail.com>
2025-07-15 13:36:10 +0530

    upadate style

--- apps/landing-page/components/Footer.tsx
+++ apps/landing-page/components/Footer.tsx
@@ -2,10 +2,8 @@ export default function Footer() {
   return (
     <div className="text-white flex justify-center items-center">
       <div className="text-gray-400">
-        <div className="flex items-center gap-2">
-          <span>© 2025 DrawSync. All rights reserved.</span>
-          <span>Privacy Policy</span>
-          <span>Terms of Service</span>
+        <div className="flex items-center gap-2 text-[10px] text-center p-2">
+          © 2025 DrawSync. All rights reserved. Privacy Policy Terms of Service
         </div>
       </div>
     </div>
